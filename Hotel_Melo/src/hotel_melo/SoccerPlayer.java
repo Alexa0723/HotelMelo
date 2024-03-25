8d801f2afa8e293ebed3f8639d5e7dabdc59baf7
ivanvaldesn <160986891+ivanvaldesn@users.noreply.github.com>
2024-03-24 22:56:21 -0500

    Add files via upload

---
+++ Hotel_Melo/src/hotel_melo/SoccerPlayer.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hotel_melo;
+
+/**
+ *
+ * @author ivane
+ */
+public class SoccerPlayer extends Guest {
+    String position;
+
+    public SoccerPlayer(String name) {
+        super(name);
+    }
+    
+    
+    @Override
+    public double getDiscount(){
+        return 0.2;
+    }
+}
---
+++ Hotel_Melo/src/hotel_melo/hotelMelo.java
@@ -0,0 +1,42 @@
+
+package hotel_melo;
+import java.util.*;
+
+
+public class hotelMelo {
+    public static void main(String[] args) {
+        
+            Random random = new Random();
+            System.out.println("Facturación por alimentos 24 de marzo de 2024 , Hotel Melo");
+            System.out.println("----------------------------------");
+    
+            
+            //Creamos los empleados
+            Recepcionist recepcionist = new Recepcionist();
+            Chef chef= new Chef("Frances");
+            Chef chefItaliano = new Chef("Italiano");
+            
+            //Creamos un cliente tipo guest
+            Guest guest1= new Guest("Pepe");
+            Inventory inventoryGuest1 = new Inventory(guest1,0) ;
+            
+            //Creamos un cliente soccerPlayer
+            SoccerPlayer soccerPlayer=new SoccerPlayer("Messi1"
+                    + "");
+            Inventory inventorySoccerPlayer = new Inventory(soccerPlayer,0);
+            
+            
+            int n= random.nextInt(3);
+            if (n == 0){
+                System.out.println("Hellow SoccerPlayer");
+                soccerPlayer.orderFood(chefItaliano,inventorySoccerPlayer,recepcionist);
+                recepcionist.generateFoodBill(inventorySoccerPlayer);
+            }
+            else{
+                System.out.println("Hello guest");
+                guest1.orderFood(chef,inventoryGuest1,recepcionist);
+                recepcionist.generateFoodBill(inventoryGuest1);
+            }
+            
+    }
+}
